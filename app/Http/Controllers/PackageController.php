89d5fc1032290d62dd100f4d393409826193d6ca
Darshit <darshit.code@gmail.com>
2026-01-28 10:56:15 +0530

    change : icons in table

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -163,7 +163,12 @@ class PackageController extends Controller
                 if ($row->status == 0) {
                     $operate .= BootstrapTableService::button('fa fa-check', '#', ['change-package-status', 'btn-gradient-success'], ['title' => trans("publish_package"), 'data-id' => $row->id]);
                 } else {
-                    $operate .= BootstrapTableService::button('fa fa-times', '#', ['change-package-status', 'btn-gradient-warning'], ['title' => trans("unpublished_package"), 'data-id' => $row->id]);
+                    $operate .= BootstrapTableService::button('<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9.16992 14.8299L14.8299 9.16992" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M14.8299 14.8299L9.16992 9.16992" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+', '#', ['change-package-status', 'btn-gradient-dark'], ['title' => trans("unpublished_package"), 'data-id' => $row->id]);
                 }
                 $operate .= BootstrapTableService::editButton(route('package.edit', $row->id), false);
                 $operate .= BootstrapTableService::deleteButton(route('package.destroy', $row->id));
