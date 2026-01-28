59ab2cd3fb98b10e824bc9e8daff372ca24796dc
sapnaCreativedemonz <sapna@creativedemonz.com>
2026-01-28 16:03:38 +0530

    parent-support

--- app/Models/ParentSupport.php
+++ app/Models/ParentSupport.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class ParentSupport extends Model
 {
     use HasFactory;
+
+     public function child()
+    {
+        return $this->belongsTo(Students::class)->withTrashed();
+    }
 }
